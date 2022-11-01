bbc7c8d1f521c5d1f4fdc3344ae6dbf50e3c5e30
Dre <dredawkins11@gmail.com>
2022-10-31 20:16:51 -0500

    completed initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles/app.scss"
+
+const App = () =>{
+    return (
+        <h1>
+            Welcome to my portfolio
+        </h1>
+    )
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        resolve: {
+          extensions: [".tsx", ".ts", ".js", ".jsx"],
+        },
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
